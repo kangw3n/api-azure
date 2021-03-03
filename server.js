c6285c26ba550513ba8aa25e42f0f16f690da418
kangw3n <weitochearyeah@hotmail.com>
2021-03-03 23:25:00 +0800

    added cron to scheduler.php

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
+
+var CronJob = require("cron").CronJob;
+var job = new CronJob(
+  "00 00 00 * * *",
+  function () {
+    fetch("https://ivftpe.com/api/scheduler.php");
+  },
+  null,
+  true,
+  "	Asia/Taipei"
+);
+job.start();
 
 app.get("/", async (req, res) => {
   return res.json({
