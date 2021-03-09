9206e57394c55a1010e7ac843b281e33e5cecb73
kangw3n <weitochearyeah@hotmail.com>
2021-03-09 13:52:31 +0800

    remove cron job

--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 
 
 ![Deploy Node.js to Azure Web App](https://github.com/kangw3n/api-azure/workflows/Deploy%20Node.js%20to%20Azure%20Web%20App/badge.svg?branch=master)
+
--- server.js
+++ server.js
@@ -1,25 +1,11 @@
-const express = require("express");
-const fetch = require("node-fetch");
+const express = require('express');
 
 const app = express();
 
-var CronJob = require("cron").CronJob;
-var job = new CronJob(
-  "00 50 23 * * *",
-  function () {
-    // console.log("test");
-    fetch("https://ivftpe.com/api/scheduler.php");
-  },
-  null,
-  true,
-  "Asia/Taipei"
-);
-job.start();
-
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   return res.json({
     state: true,
-    message: "API-Services Available 2",
+    message: 'API-Services Available 2',
   });
 });
 
